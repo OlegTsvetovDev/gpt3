9d773b36a9003a7d26a8e47fbe97cd8ec547fcac
cov <brainstormmedia@ya.ru>
2022-11-04 20:05:07 +0500

    Added transitions.

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './features.css'
 
 
+const data = {
+  header: 'The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.',
+  button: 'Request Early Access to Get Started'
+}
+
 const Features = ({ props }) => (
-  <div>
-    Features
+  <div className="gpt3__features section__padding">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">{data.header}</h1>
+      <a href="">{data.button}</a>
+    </div>
   </div>
 )
 
