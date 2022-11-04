b7783e89f9f14259d82f4515e4f9840077472853
cov <brainstormmedia@ya.ru>
2022-11-04 19:46:22 +0500

    What is GDP done.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import './feature.css'
 
 
-const Feature = ({ props }) => (
-  <div>
-    Feature
-  </div>
-)
+const Feature = ({ title, text, layout }) => {
+  const orientation = (layout === 'column') ? ' column' : ' row'
+  return (
+    <div className={"gpt3__features-container__feature" + orientation}>
+      <div className="gpt3__features-container__feature-title">
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  )
+}
 
 
 export default Feature
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import Feature from '../../components/feature/Feature'
 import './whatGPT3.css'
 
 
-const WhatGPT3 = ({ props }) => (
+const data = [
+  {
+    id: 0,
+    title: 'What is GPT-3',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+  },
+  {
+    id: 1,
+    title: 'Chatbots',
+    text: 'We so opinion friends me message as delight. Whole front do of plate heard oh ought.'
+  },
+  {
+    id: 2,
+    title: 'Knowledgebase',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+  },
+  {
+    id: 3,
+    title: 'Education',
+    text: 'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b'
+  },
+]
+
+const topFeature = data[0]
+const otherFeatures = data.slice(1)
+
+const WhatGPT3 = () => (
   <div>
-    WhatGPT3
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature {...topFeature} layout="row" />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <a href="#">Explore The Library</a>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        {
+          otherFeatures.map(feature =>
+            <Feature key={feature.id} {...feature} layout="column" />
+          )
+        }
+      </div>
+    </div>
   </div>
 )
 
