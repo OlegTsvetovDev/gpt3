75847e7e7abffc42d0e4be64a4b900628ca3dc1d
cov <brainstormmedia@ya.ru>
2022-11-05 14:34:27 +0500

    Possibility container done.

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import possibility from '../../assets/possibility.png'
 import './possibility.css'
 
 
-const Possibility = ({ props }) => (
-  <div>
-    Possibility
+const Possibility = () => (
+  <div className="gpt3__possibility section__padding" id="possibility">
+    <div className="gpt3__possibility-image">
+      <img src={possibility} alt="gpt3-possibilities"/>
+    </div>
+    <div className="gpt3__possibility-content">
+      <a href="/">Request Early Access to Get Started</a>
+      <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <a href="/">Request Early Access to Get Started</a>
+    </div>
   </div>
 )
 
