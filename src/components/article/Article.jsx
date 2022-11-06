0fa3fcf5166ab340291fe90e6c016f59fa9a1307
cov <brainstormmedia@ya.ru>
2022-11-06 12:55:22 +0500

    Blog container done.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './article.css'
 
 
-const Article = ({ props }) => (
-  <div>
-
+const Article = ({ date, header, img }) => (
+  <div className="gpt3__blog-container_article">
+    <div className="gpt3__blog-container_article-img">
+      <img src={img} alt="header" />
+    </div>
+    <div className="gpt3__blog-container_article-content">
+      <p>{date}</p>
+      <h3>{header}</h3>
+      <a href="/">Read full Article</a>
+    </div>
   </div>
 )
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import { Article } from '../../components/'
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css'
 
 
-const Blog = ({ props }) => (
-  <div id="blog">
-    Blog
+const mainArticleData = {
+  id: 0,
+  date: 'Sep 26, 2021',
+  header: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+  img: blog01
+}
+
+const articleData = [
+  {
+    id: 1,
+    date: 'Sep 26, 2021',
+    header: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    img: blog02
+  },
+  {
+    id: 2,
+    date: 'Sep 26, 2021',
+    header: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    img: blog03
+  },
+  {
+    id: 3,
+    date: 'Sep 26, 2021',
+    header: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    img: blog04
+  },
+  {
+    id: 4,
+    date: 'Sep 26, 2021',
+    header: 'GPT-3 and Open  AI is the future. Let us exlore how it is?',
+    img: blog05
+  },
+]
+
+
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">A lot is happening,<br />We are blogging about it.</h1>
+    </div>
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-container__groupA">
+        <Article type="primary" {...mainArticleData} />
+      </div>
+      <div className="gpt3__blog-container__groupB">
+        {
+          articleData.map(article =>
+            <Article key={article.id} {...article} type="secondary" />
+          )
+        }
+      </div>
+    </div>
   </div>
 )
 
